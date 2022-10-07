d6fab8388e7d1b5b64da14bf18968fde90e2edc0
qc-one <540595510@qq.com>
2022-10-07 14:45:51 +0800

    event-bus

--- vue_project/src/App.vue
+++ vue_project/src/App.vue
@@ -1,5 +1,9 @@
 <template>
     <div id="app">
+        <h1>组件传参</h1>
+        <Father></Father>
+        <Son></Son>
+        <a-divider />
         <a-button type="primary" @click="keepAlice('testOne')">testOne切换组件</a-button>
         <a-button type="primary" @click="keepAlice('testTwo')">testTwo切换组件</a-button>
         <keep-alive exclude="KeepTest">
@@ -11,12 +15,16 @@
 <script>
     import KeepAlive from './components/KeepAlive.vue'
     import KeepTest from './components/Test.vue'
+    import Father from './components/Father.vue'
+    import Son from './components/Son.vue'
 
     export default {
         name: 'App',
         components: {
             testOne: KeepAlive,
-            testTwo: KeepTest
+            testTwo: KeepTest,
+            Father,
+            Son
         },
         data() {
             return {
---
+++ vue_project/src/components/Son.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="son">
+        son
+        <a-button @click="sayFu" sayFu>父组件</a-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SonSon',
+        created() {
+            console.log('son', this.$EventBus);
+        },
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+            sayFu() {
+                this.$EventBus.$emit("getData", 123);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
