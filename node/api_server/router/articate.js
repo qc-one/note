34995b73b48e56f8329c35e8fac2dd6a0403cb8d
qc-one <540595510@qq.com>
2023-04-09 22:18:36 +0800

    新增文章分类

--- node/api_server/router/articate.js
+++ node/api_server/router/articate.js
@@ -2,12 +2,19 @@
 
 const express = require('express');
 // 导入用户路由处理函数模块
-const artHandler = require('../router_handler/articate')
+const artcate_handler = require('../router_handler/articate')
+const expressJoi = require('@escook/express-joi')
+// 导入文章分类的验证模块
+const {
+    add_cate_schema
+} = require('../schema/artcate')
 
 // 创建路由对象
 const router = express.Router();
 // 获取文章分类的列表数据
-router.get('/cates', artHandler.getArticate)
+router.get('/cates', artcate_handler.getArticate)
+// 新增文章分类的路由
+router.post('/addcates', expressJoi(add_cate_schema), artcate_handler.addArticleCates)
 
 // 将路由对象共享出去
 module.exports = router
--- node/api_server/router_handler/articate.js
+++ node/api_server/router_handler/articate.js
@@ -19,3 +19,29 @@ exports.getArticate = (req, res) => {
         })
     })
 }
+// 新增文章分类的处理函数
+exports.addArticleCates = (req, res) => {
+    // 定义查询 分类名称 与 分类别名 是否被占用的 SQL 语句
+    const sql = `select * from ev_article_cate where name=? or alias=?`
+    // 执行查重操作
+    db.query(sql, [req.body.name, req.body.alias], (err, results) => {
+        // 执行 SQL 语句失败
+        if (err) return res.cc(err)
+        // 分类名称 和 分类别名 都被占用
+        if (results.length === 2) return res.cc('分类名称与别名被占用，请更换后重试！')
+        if (results.length === 1 && results[0].name === req.body.name && results[0].alias === req.body.alias) return res.cc('分类名称与别名被占用，请更换后重试！')
+        // 分类名称 或 分类别名 被占用
+        if (results.length === 1 && results[0].name === req.body.name) return res.cc('分类名称被占用，请更换后重试！')
+        if (results.length === 1 && results[0].alias === req.body.alias) return res.cc('分类别名被占用，请更换后重试！')
+        // TODO：新增文章分类
+        const sql = `insert into ev_article_cate set ?`;
+        db.query(sql, req.body, (err, results) => {
+            // SQL 语句执行失败
+            if (err) return res.cc(err)
+            // SQL 语句执行成功，但是影响行数不等于 1
+            if (results.affectedRows !== 1) return res.cc('新增文章分类失败！')
+            // 新增文章分类成功
+            res.cc('新增文章分类成功！', 0)
+        })
+    })
+}
