52bb09da07653601f1478f5a76524e35d7168351
qc-one <540595510@qq.com>
2023-04-11 22:07:10 +0800

    根据id获取文章分类数据

--- node/api_server/router/articate.js
+++ node/api_server/router/articate.js
@@ -7,7 +7,8 @@ const expressJoi = require('@escook/express-joi')
 // 导入文章分类的验证模块
 const {
     add_cate_schema,
-    delete_cate_schema
+    delete_cate_schema,
+    get_cate_schema
 } = require('../schema/artcate')
 
 // 创建路由对象
@@ -18,6 +19,8 @@ router.get('/cates', artcate_handler.getArticate)
 router.post('/addcates', expressJoi(add_cate_schema), artcate_handler.addArticleCates)
 // 删除文章分类的路由
 router.get('/deletecate/:id', expressJoi(delete_cate_schema), artcate_handler.deleteCateById)
+// 根据ID获取文章分类的路由
+router.get('/cates/:id', expressJoi(get_cate_schema), artcate_handler.getArtCateById)
 
 // 将路由对象共享出去
 module.exports = router
