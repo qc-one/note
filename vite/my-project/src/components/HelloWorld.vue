76a7c8cc89840c71261f1cc34ae81ab76bf1f174
qin <540595510@qq.com>
2024-03-07 17:05:17 +0800

    【0307】-【vit】

--- vite/my-project/src/components/HelloWorld.vue
+++ vite/my-project/src/components/HelloWorld.vue
@@ -4,38 +4,24 @@ import { ref } from "vue";
 defineProps({
     msg: String,
 });
+console.log("import.meta.env::", import.meta.env);
 
 const count = ref(1);
 </script>
 
 <template>
     <h1>{{ msg }}</h1>
-
-    <div class="card">
-        <button type="button" @click="count++">count is {{ count }}</button>
-        <p>
-            Edit
-            <code>components/HelloWorld.vue</code> to test HMR
-        </p>
+    <div class="box">
+        <p class="red">12345</p>
     </div>
-
-    <p>
-        Check out
-        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-            >create-vue</a
-        >, the official Vue + Vite starter
-    </p>
-    <p>
-        Install
-        <a href="https://github.com/vuejs/language-tools" target="_blank"
-            >Volar</a
-        >
-        in your IDE for a better DX
-    </p>
-    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
+.box {
+    .red {
+        color: red;
+    }
+}
 .read-the-docs {
     color: #888;
 }
