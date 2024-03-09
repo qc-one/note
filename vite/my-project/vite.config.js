893a69ce39865cda47369f28e58aabc4249ebd41
qin <540595510@qq.com>
2024-03-09 20:27:29 +0800

    【0309】-【vite低版本浏览器兼容】

--- vite/my-project/vite.config.js
+++ vite/my-project/vite.config.js
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({  
+      targets: ['firefox > 40', 'chrome >= 52'],  
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],  
+      renderLegacyChunks: true,  
+      polyfills: [  
+        'es.symbol',  
+        'es.promise',  
+        'es.promise.finally',  
+        'es/map',  
+        'es/set',  
+        'es.array.filter',  
+        'es.array.for-each',  
+        'es.array.flat-map',  
+        'es.object.define-property',  
+        // ...其他需要的polyfills  
+      ],  
+    }), 
+  ],
   base: '/admin',
+
 })
