02dc2f68f52695621894b9d427cc02c3f6ad10f8
qc-one <540595510@qq.com>
2024-05-23 21:38:37 +0800

    【0523】-【TS】

---
+++ TS/subscribe.ts
@@ -0,0 +1,15 @@
+
+document.addEventListener("click", () => {
+    console.log("点击了")
+})
+
+document.addEventListener("test", () => {
+    console.log("自定义事件")
+}, {
+    once: true
+})
+const e = new Event('test')
+document.dispatchEvent(e)
+document.dispatchEvent(e)
+document.dispatchEvent(e)
+document.dispatchEvent(e)
