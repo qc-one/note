53707ae930d3b5b09949267dfe2bd36e8b235e98
qc-one <540595510@qq.com>
2024-06-01 23:02:56 +0800

    【0601】-【vite识别vue文件】

--- vite/vite-dev-server/index.js
+++ vite/vite-dev-server/index.js
@@ -7,13 +7,23 @@ const path = require("path")
 const app = new koa()
 
 app.use(async (ctx) => {
-    console.log("ctx", ctx.request, ctx.response)
+    // console.log("ctx", ctx.request, ctx.response)
     const req = ctx.request
     const res = ctx.response
     if (req.url === '/') {
         const indexContent = await fs.promises.readFile(path.resolve(__dirname, "./index.html"), "utf-8")
         res.body = indexContent
         res.set("Content-Type", "text/html")
+    }
+    if (req.url === "/src/main.js") {
+        const mainContent = await fs.promises.readFile(path.resolve(__dirname, "./src/main.js"), "utf-8")
+        res.body = mainContent
+        res.set("Content-Type", "text/javascript")
+    }
+    if (req.url === "/src/App.vue") {
+        const vueContent = await fs.promises.readFile(path.resolve(__dirname, "./src/App.vue"))
+        res.body = vueContent
+        res.set("Content-Type", "text/javascript")
     }
 })
 
