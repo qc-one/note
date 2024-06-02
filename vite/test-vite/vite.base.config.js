30f282c8a57f204e0fdc319a3fba019aaa96208b
qc-one <540595510@qq.com>
2024-06-02 15:16:10 +0800

    【0602】-【css配置】

--- vite/test-vite/vite.base.config.js
+++ vite/test-vite/vite.base.config.js
@@ -4,5 +4,27 @@ export default defineConfig({
     //     optimizeDeps: {
     //         // exclude: ['lodash-es'], // 当遇到lodash-es时，不进行tree shaking预构建，将指定数组中的依赖不进行依赖预构建
     //     },
-    envPrefix: 'ENV_'
+    envPrefix: 'ENV_',
+    // 对css进行配置
+    css: {
+        // 是对css模块化的默认行为进行覆盖，所有的配置最终会丢给postcss-modules插件
+        modules: {
+            // 修改生成类名的展示形式（驼峰还是中划线形式），默认是dashesOnly，值有camelCase(驼峰)、camelCaseOnly(只有驼峰)、dashes(中划线)、dashesOnly(只有中划线)
+            // localsConvention: 'camelCaseOnly',
+            // 配置当前的模块化行为，默认是global，值有local(局部)、global(全局)
+            // scopeBehaviour: 'local',
+            // 生成类名的规则，指定css文件中的变量名，默认是类名
+            // generateScopedName: '[name]__[local]___[hash:base64:5]',
+            // generateScopedName: (name, filename, css) => {
+            //     // name：代表的是css文件中变量名
+            //     // filename：代表的是css文件名
+            //     // css：代表的是css文件内容
+            //     return '[name]__[local]___[hash:base64:5]'
+            // }
+            // 配置是否生成hash前缀，默认是true
+            // hashPrefix: 'vite-'
+            // 代表不想参与模块化的文件，默认是空数组
+            // globalModulePaths: ['./componentA.module.css']
+        },
+    }
 })
